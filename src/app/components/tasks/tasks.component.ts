c8df0872541acc6b25ff1eeb6d7c3e07973463c3
Piliansa <pilaransa31@gmail.com>
2022-05-31 18:18:00 -0300

    task-list c icono

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Task } from 'src/app/task';
 import { TASKS } from '../../mock-tasks';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-tasks',
@@ -9,7 +10,7 @@ import { TASKS } from '../../mock-tasks';
 })
 export class TasksComponent implements OnInit {
   tasks: Task[] = TASKS;
-
+  faTimes = faTimes;
   constructor() { }
 
   ngOnInit(): void {
