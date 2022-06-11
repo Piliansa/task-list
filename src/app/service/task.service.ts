a5d51b60eecc734ac31e8c1421ba91d405601b46
Piliansa <pilaransa31@gmail.com>
2022-06-11 20:42:51 -0300

    subiendo trabajo hecho hasta ahora 11/06

--- src/app/service/task.service.ts
+++ src/app/service/task.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHandler} from '@angular/common/http';
-import { Observable, of } from 'rxjs';
+import { BehaviorSubject, Observable, of } from 'rxjs';
 import { Task } from 'src/app/task';
 import {TASKS} from 'src/app/mock-tasks'
 
@@ -16,7 +16,7 @@ export class TaskService {
   getTasks(): Observable<Task[]> {
     return this.http.get<Task[]>(this.apiUrl)
   }
-  deleteTask(task:Task): Observable<Any>{
+  deleteTask(task:Task): Observable<Task>{
   const url = `${this.apiUrl}/${task.id}`;
   return this.http.delete<Task>{url};
   }
